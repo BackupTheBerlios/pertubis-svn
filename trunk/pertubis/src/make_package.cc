7c959bce922fea1d4d43841c04b3142467402240
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-02 18:29:07 +0000

    I'm back part 2

---
+++ trunk/pertubis/src/make_package.cc
@@ -0,0 +1,77 @@
+
+/* Copyright (C) 2007-2008 Stefan Koegl
+*
+* This file is part of pertubis
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#include "make_package.hh"
+#include "Package.hh"
+
+pertubis::Package*
+pertubis::makePackage(paludis::tr1::shared_ptr<const paludis::PackageID> id,
+                                        Qt::CheckState install,
+                                        Qt::CheckState deinstall,
+                                        QString package,
+                                        QString cat,
+                                        Qt::CheckState isInstalled,
+                                        PackageState mystate,
+                                        Package* pitem,
+                                        QString mask_reasons)
+{
+    Package* pack(new Package(id,QVector<QVariant>(pho_last),mystate,pt_parent,pitem));
+    pack->setData(pho_install,install);
+    pack->setData(pho_deinstall,deinstall);
+    pack->setData(pho_package,package);
+    pack->setData(pho_category,cat);
+    pack->setData(pho_repository, "");
+    pack->setData(pho_installed,QVariant(static_cast<int>(isInstalled)));
+    pack->setData(pho_mask_reasons,mask_reasons);
+    return pack;
+}
+
+pertubis::Package*
+pertubis::makeRootPackage()
+{
+    return new Package(paludis::tr1::shared_ptr<const paludis::PackageID>(),
+                       QVector<QVariant>(pho_last),
+                       ps_stable,
+                       pt_parent,
+                       0);
+}
+
+pertubis::Package*
+pertubis::makeVersionPackage(paludis::tr1::shared_ptr<const paludis::PackageID> id,
+        Qt::CheckState install,
+        Qt::CheckState deinstall,
+        QString version,
+        QString rep,
+        Qt::CheckState isInstalled,
+        PackageState mystate,
+        Package* pitem,
+        QString mask_reasons)
+{
+    Package* pack(new Package(id,QVector<QVariant>(pho_last),mystate,pt_child,pitem));
+    pack->setData(pho_install,install);
+    pack->setData(pho_deinstall,deinstall);
+    pack->setData(pho_package,version);
+    pack->setData(pho_category,"");
+    pack->setData(pho_repository, rep);
+    pack->setData(pho_installed,QVariant(static_cast<int>(isInstalled)));
+    pack->setData(pho_mask_reasons,mask_reasons);
+    return pack;
+}
---
+++ trunk/pertubis/src/make_package.hh
@@ -0,0 +1,67 @@
+
+/* Copyright (C) 2007 Stefan Koegl
+*
+* This file is part of pertubis
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef _PERTUBIS_ENTRY_PROTECTOR_MAKE_PACKAGE_H
+#define _PERTUBIS_ENTRY_PROTECTOR_MAKE_PACKAGE_H 1
+
+#include "Package-fwd.hh"
+#include <Qt>
+#include <paludis/package_id-fwd.hh>
+#include <paludis/util/tr1_memory.hh>
+
+namespace pertubis
+{
+
+    /** \brief creates a node to diplay a package, can have subnodes
+     * \ingroup PackageModelClass
+     *
+     */
+    Package* makePackage(paludis::tr1::shared_ptr<const paludis::PackageID> id,
+                         Qt::CheckState install,
+                         Qt::CheckState deinstall,
+                         QString pack,
+                         QString cat,
+                         Qt::CheckState isInstalled,
+                         PackageState mystate,
+                         Package* myparent,
+                         QString mask_reasons);
+
+    /** \brief creates a subnode to display a version
+     * \ingroup PackageModelClass
+     *
+     */
+    Package* makeVersionPackage(paludis::tr1::shared_ptr<const paludis::PackageID> id,
+                                Qt::CheckState install,
+                                Qt::CheckState deinstall,
+                                QString version,
+                                QString rep,
+                                Qt::CheckState isInstalled,
+                                PackageState mystate,
+                                Package* pitem,
+                                QString mask_reasons);
+
+    /** \brief creates a simple root Package instance
+     * \ingroup PackageModelClass
+     *
+     */
+    Package* makeRootPackage();
+}
+
+#endif
