23c41923b4acac573429e0d3b8257fb3bbac7c6d
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:06:44 +0000

    * in 'SelectionModel.cc' replaced text

--- trunk/pertubis/src/SelectionModel.cc
+++ trunk/pertubis/src/SelectionModel.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007-2008 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 #include <QBrush>
 #include <QVariant>
 
-using namespace pertubis;
+using namespace perturbis;
 
 SelectionModel::SelectionModel(QObject* pobj) : PackageModel(pobj)
 {
