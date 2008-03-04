4511195e32f7750983e290bcf3c6682b5fecc28f
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:07:12 +0000

    * in 'SearchThread.cc' replaced text

--- trunk/pertubis/src/SearchThread.cc
+++ trunk/pertubis/src/SearchThread.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007-2008 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -53,7 +53,7 @@
 #include <QStringList>
 #include <set>
 
-using namespace pertubis;
+using namespace perturbis;
 
 struct Matches
 {
