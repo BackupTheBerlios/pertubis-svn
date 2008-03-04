82c7a3de0fd14066068b257a7cc6e00628773736
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:06:01 +0000

    * in 'RegexMatcher.cc' replaced text

--- trunk/pertubis/src/RegexMatcher.cc
+++ trunk/pertubis/src/RegexMatcher.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007-2008 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@
 #include "RegexMatcher.hh"
 
 
-using namespace pertubis;
+using namespace perturbis;
 
 RegexMatcher::RegexMatcher(const std::string& pattern) : m_regex(QString::fromStdString(pattern))
 {
