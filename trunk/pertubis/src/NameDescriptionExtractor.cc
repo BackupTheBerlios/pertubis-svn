676fd084c738208bf0691f256de84f20067c7d7c
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:07:52 +0000

    * in 'NameDescriptionExtractor.cc' replaced text

--- trunk/pertubis/src/NameDescriptionExtractor.cc
+++ trunk/pertubis/src/NameDescriptionExtractor.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 #include <paludis/util/stringify.hh>
 #include <paludis/metadata_key.hh>
 
-using namespace pertubis;
+using namespace perturbis;
 
 NameDescriptionExtractor::NameDescriptionExtractor(const paludis::Environment * const env) : Extractor(env)
 {
