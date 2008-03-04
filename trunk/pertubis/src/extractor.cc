95e01a30a16b9d999573b56c8ea81c824c23989e
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:10:06 +0000

    * in 'extractor.cc' replaced text

--- trunk/pertubis/src/extractor.cc
+++ trunk/pertubis/src/extractor.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 
 #include "extractor.hh"
 
-using namespace pertubis;
+using namespace perturbis;
 
 Extractor::Extractor(const paludis::Environment * const env) : m_env(env)
 {
