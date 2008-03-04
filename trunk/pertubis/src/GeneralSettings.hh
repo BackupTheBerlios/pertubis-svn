ef3592abebf8e4552f090203da9afe1a71cc78bc
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:07:42 +0000

    * in 'GeneralSettings.hh' replaced text

--- trunk/pertubis/src/GeneralSettings.hh
+++ trunk/pertubis/src/GeneralSettings.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ class QSpinBox;
 class QCheckBox;
 class QFontComboBox;
 
-namespace pertubis
+namespace perturbis
 {
     class GeneralSettingsModel : public QObject
     {
