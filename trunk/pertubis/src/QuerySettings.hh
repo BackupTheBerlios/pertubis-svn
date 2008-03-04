37e1d9e01b4f09168e51cfcffa00e0311159f85a
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:06:57 +0000

    * in 'QuerySettings.hh' replaced text

--- trunk/pertubis/src/QuerySettings.hh
+++ trunk/pertubis/src/QuerySettings.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@
 class QComboBox;
 class QButtonGroup;
 
-namespace pertubis
+namespace perturbis
 {
     class QuerySettingsModel : public QObject
     {
