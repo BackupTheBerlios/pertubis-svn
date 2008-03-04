1c912b832fab16bb0140b4c6318e3b5dafaa03a1
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:05:16 +0000

    * in 'text_matcher.hh' replaced text

--- trunk/pertubis/src/text_matcher.hh
+++ trunk/pertubis/src/text_matcher.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 
 #include "matcher.hh"
 
-namespace pertubis
+namespace perturbis
 {
     /// case independent substring matching
     class TextMatcher : public Matcher
