69c8dd32acaabc36d63bbb96dd9768a1ca7caf8d
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:05:44 +0000

    * in 'ThreadBase.hh' replaced text

--- trunk/pertubis/src/ThreadBase.hh
+++ trunk/pertubis/src/ThreadBase.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@
 #include <paludis/environment-fwd.hh>
 #include <paludis/package_id-fwd.hh>
 
-namespace pertubis
+namespace perturbis
 {
 
     /*! \brief provides the paludis infrastructure we need for the threads
