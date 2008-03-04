80435e4dbf6118b15593e6dfe64f0b204099b74c
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:11:23 +0000

    * in 'ItemTest.hh' replaced text

--- trunk/pertubis/test/ItemTest.hh
+++ trunk/pertubis/test/ItemTest.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl <hotshelf@users.berlios.de>
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 #include <QtCore>
 #include <QtGui>
 
-namespace pertubis
+namespace perturbis
 {
 	class TaskBox;
 
