c08ec20660121f852f55dc83d3c2ecc3fb6d0126
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:08:25 +0000

    * in 'SelectionModel.hh' replaced text

--- trunk/pertubis/src/SelectionModel.hh
+++ trunk/pertubis/src/SelectionModel.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 #include "PackageModel.hh"
 #include <QStringList>
 
-namespace pertubis
+namespace perturbis
 {
 
     /*! \brief holds package data fetched from paludis in a tree structure
