bb5dcf61bd6e856cad3d017aad95ba7ff9678ad4
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:06:04 +0000

    * in 'HtmlFormatter.cc' replaced text

--- trunk/pertubis/src/HtmlFormatter.cc
+++ trunk/pertubis/src/HtmlFormatter.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007-2008 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 #include <paludis/name.hh>
 #include "FormatterUtils.hh"
 
-using namespace pertubis;
+using namespace perturbis;
 
 HtmlFormatter::HtmlFormatter() :
         new_use_flag_names(new paludis::UseFlagNameSet),
