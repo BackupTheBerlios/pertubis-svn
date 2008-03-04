144129acfd6d1922bc3c8efe98d7ff9aa458de69
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:06:22 +0000

    * in 'matcher.cc' replaced text

--- trunk/pertubis/src/matcher.cc
+++ trunk/pertubis/src/matcher.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 
 #include "matcher.hh"
 
-using namespace pertubis;
+using namespace perturbis;
 
 Matcher::Matcher()
 {
