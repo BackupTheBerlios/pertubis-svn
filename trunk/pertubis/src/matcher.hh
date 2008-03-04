3affd3a14a4b036bcc34e7eaea8a815a86fc2d0c
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:05:19 +0000

    * in 'matcher.hh' replaced text

--- trunk/pertubis/src/matcher.hh
+++ trunk/pertubis/src/matcher.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 
 #include <string>
 
-namespace pertubis
+namespace perturbis
 {
     /*! \brief abstract base class of a class that tests strings for some conditions
      * \ingroup PaludisAdapter
