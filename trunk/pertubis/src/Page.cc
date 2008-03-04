39c39a24c213f9f0b86dc0188a5f6ae5b8fc659e
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:07:21 +0000

    * in 'Page.cc' replaced text

--- trunk/pertubis/src/Page.cc
+++ trunk/pertubis/src/Page.cc
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007-2008 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 #include "Page.hh"
 #include "MainWindow.hh"
 
-using namespace pertubis;
+using namespace perturbis;
 
 struct PagePrivate
 {
