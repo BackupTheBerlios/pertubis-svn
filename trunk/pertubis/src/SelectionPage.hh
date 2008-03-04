765d383fbbac3a1a17c1a2d8b93e42add0306a69
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:09:51 +0000

    * in 'SelectionPage.hh' replaced text

--- trunk/pertubis/src/SelectionPage.hh
+++ trunk/pertubis/src/SelectionPage.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ class QModelIndex;
 
 
 
-namespace pertubis
+namespace perturbis
 {
     struct SelectionPagePrivate;
 
