44e3c7e70b9af4bec718e26cf9534b4a7df526bc
hotshelf <hotshelf@b199f59f-2b32-0410-ac87-e47f08239bfc>
2008-03-04 14:07:58 +0000

    * in 'ReportModelDelegate.hh' replaced text

--- trunk/pertubis/src/ReportModelDelegate.hh
+++ trunk/pertubis/src/ReportModelDelegate.hh
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2007 Stefan Koegl
 *
-* This file is part of pertubis
+* This file is part of perturbis
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ class QSortFilterProxyModel;
 
 #include <QSize>
 
-namespace pertubis
+namespace perturbis
 {
 
     /*! \brief Used for presenting a customized selections display in PackageView
